0d5182f0cb7ebac6f4eb78033938b3e8a8c3e9a6
Sriram <sriramsivakumar23@gmail.com>
2023-08-09 16:17:23 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Home from './src/Screens/Home';
+import Signin from './src/Screens/Signin';
+import SigninContainer from './src/Screens/SigninContainer';
+import ChatScreen from './src/Screens/ChatScreen'
+
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'rgba(105, 101, 251, 0.5)', 
+          },
+          headerTintColor: 'white', 
+          headerTitleStyle: {
+            fontWeight: 'bold', 
+          },
+        }}
+      >
+        <Stack.Screen 
+        component={Signin}
+        name='signin'
+        options={{headerShown:false}}
+        />
+
+        <Stack.Screen name='SigninContainer' component={SigninContainer} options={{headerShown:false}}/>
+        
+        <Stack.Screen name='Home' component={Home} options={{headerShown:false}}/>
+        <Stack.Screen name='chatScreen' component={ChatScreen} />
+      </Stack.Navigator>
+
+      
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
